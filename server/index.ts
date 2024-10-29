c12a6778df7e1b67431d706d543a2b5e5c9f71e0
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-10-29 13:53:49 +0200

    Add cookieParser to index

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,12 @@ import express, { Express } from 'express'
 import chalk from "chalk";
 import router from "./router/router";
 import "dotenv/config";
+import cookieParser from 'cookie-parser';
 
 const app : Express = express();
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use(router);
 
 
