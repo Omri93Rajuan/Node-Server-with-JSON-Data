1892b805e596ac771eae30d8709d157d1ed609ec
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-10-30 15:34:48 +0200

    Remove commits

--- server/middleware/jwt.ts
+++ server/middleware/jwt.ts
@@ -1,12 +1,11 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import { User } from '../data/model/dataModel';
+import  User  from '../data/model/dataModel';
 
-// מפתח הסוד לאימות
 const key: string = process.env.JWT_SECRET || 'default_secret';
 
 const generateAuthToken = (user: User): string => {
-  const { id, isBusiness, isAdmin } = user;
-  const token = jwt.sign({ id, isBusiness, isAdmin }, key);
+  const { id, isAdmin } = user;
+  const token = jwt.sign({ id, isAdmin }, key);
   return token;
 };
 
